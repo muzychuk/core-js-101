718a5f146bdd27bd1569a5149703b71fc1ca30ba
Andrii Muzychuk <andrii.muzychuk1991@gmail.com>
2021-06-16 23:40:29 +0200

    fix: lint error

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -581,7 +581,7 @@ function getElementByIndexes(arr, indexes) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(arr) {
+function swapHeadAndTail(/* arr */) {
   throw new Error('Not implemented');
 }
 
